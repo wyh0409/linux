1612eb4ecdbc386fa93fec23a6673e19100e6181
jirnpsyche <38399625+jirnpsyche@users.noreply.github.com>
2019-06-03 15:43:20 +0800

    Update myjava.java

--- java/part 1~2/myjava.java
+++ java/part 1~2/myjava.java
@@ -2,6 +2,7 @@ public class myjava
 {
     public static void main(String args[])
     {
+        /*2323232323*/
         System.out.println("let us begin to study java");
        /* system.out.println("study java");*/
     }
